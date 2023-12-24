06e99a5fd89b0a893950dc836612951e18c86334
Gowri K <gowrikalimuthu2003@gmail.com>
2023-12-24 17:37:58 +0530

    Nav added

---
+++ my-app/src/Component/Head/Header.js
@@ -0,0 +1,52 @@
+import React,{ useState } from 'react'
+import "./Header.css"
+import logo from "../Head/pic/logo1.png"
+
+const Header = () => {
+//when scroll header at top
+  window.addEventListener("scroll", function(){
+    const header = document.querySelector(".header")
+    header.classList.toggle("active", window.scrollY > 100)
+  })
+
+//toggle menu
+const [Mobile,setMobile] = useState(false)
+
+
+  return (
+  <>
+  <header className='header'>
+    <div className="container d_flex">
+        <div className="logo">
+            <img className="pho" src={logo} alt="" />
+        </div>
+
+        <div className='navlink'>
+            {/* <ul className='link f_flex uppercase'> */}
+              <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#features'>Features</a></li>
+                <li><a href='#portfolio'>Portfolio</a></li>
+                <li><a href='#resume'>Resume</a></li>
+                <li><a href='#clients'>Clients</a></li>
+                <li><a href='#blog'>Blog</a></li>
+                <li><a href='#contact'>Contact</a></li>
+                <li><button className='home-btn'>BUY NOW</button></li>
+            </ul>
+
+            <button className='toggle' onClick={() => setMobile(!Mobile)}>
+              {Mobile ?
+                <i className='bx bx-x close home-btn'></i> :  <i className='bx bx-menu open '></i>
+            }
+           
+          
+            </button>
+        </div>
+    </div>
+  </header>
+    </>
+  
+  )
+}
+
+export default Header
