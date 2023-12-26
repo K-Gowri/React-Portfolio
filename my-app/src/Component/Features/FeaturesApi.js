23b45bfff11660d0dd3940d2568fafcf34ceae57
Gowri K <gowrikalimuthu2003@gmail.com>
2023-12-27 00:34:46 +0530

    Media query added

--- my-app/src/Component/Features/FeaturesApi.js
+++ my-app/src/Component/Features/FeaturesApi.js
@@ -5,19 +5,19 @@ const data = [
     id : 1,
     image :"pic/code-alt-regular-24.png" ,
     title : "Frontend Projects",
-    desc: "  With a passion for crafting visually appealing and user-friendly interfaces, I am eager to dive into the world of frontend development. My goal is to contribute to projects that not only meet functional requirements but also provide a seamless and enjoyable user experience. I am well-versed in HTML, CSS, and JavaScript, and I am committed to staying updated on the latest trends and technologies in frontend development.",
+    desc: "  ommitted to staying updated on the latest trends and technologies in frontend development.",
 },
 {
     id : 2,
     image :"pic/wifi.png" ,
     title : "Frontend Projects",
-    desc: "  With a passion for crafting visually appealing and user-friendly interfaces, I am eager to dive into the world of frontend development. My goal is to contribute to projects that not only meet functional requirements but also provide a seamless and enjoyable user experience. I am well-versed in HTML, CSS, and JavaScript, and I am committed to staying updated on the latest trends and technologies in frontend development.",
+    desc: " ds and technologies in frontend development.",
 },
 {
     id : 3,
     image :"pic/figma.png" ,
     title : "Frontend Projects",
-    desc: "  With a passion for crafting visually appealing and user-friendly interfaces, I am eager to dive into the world of frontend development. My goal is to contribute to projects that not only meet functional requirements but also provide a seamless and enjoyable user experience. I am well-versed in HTML, CSS, and JavaScript, and I am committed to staying updated on the latest trends and technologies in frontend development.",
+    desc: "   the world of frontend develonal requiremen"
 }
 
 ]
